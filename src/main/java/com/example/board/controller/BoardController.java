29be0e446240381cf17c799d0789ff8120fdbb00
TakanoriShima <quark2galaxy@gmail.com>
2024-02-14 11:31:09 +0900

    投稿完了

--- src/main/java/com/example/board/controller/BoardController.java
+++ src/main/java/com/example/board/controller/BoardController.java
@@ -1,10 +1,18 @@
 package com.example.board.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.board.entity.Post;
 import com.example.board.factory.PostFactory;
+import com.example.board.repository.PostRepository;
 
 /**
  * 掲示板のフロントコントローラー.
@@ -12,15 +20,49 @@ import com.example.board.factory.PostFactory;
 @Controller
 public class BoardController {
 
-    /**
-    * 一覧を表示する。
-    *
-    * @param model モデル
-    * @return テンプレート
-    */
-    @GetMapping("/")
-    public String index(Model model) {
-        model.addAttribute("form", PostFactory.newPost());
-        return "layout";
-    }
+	@Autowired
+	private PostRepository repository;
+
+	/**
+	* 一覧を表示する。
+	*
+	* @param model モデル
+	* @return テンプレート
+	*/
+	@GetMapping("/")
+	public String index(Model model) {
+		model.addAttribute("form", PostFactory.newPost());
+		model = this.setList(model);
+		model.addAttribute("path", "create");
+		return "layout";
+	}
+
+	/**
+	 * 登録する。
+	 *
+	 * @param form  フォーム
+	 * @param model モデル
+	 * @return テンプレート
+	 */
+	@PostMapping("/create")
+	public String create(@ModelAttribute("form") Post form, BindingResult result,
+			Model model) {
+		repository.saveAndFlush(PostFactory.createPost(form));
+		model.addAttribute("form", PostFactory.newPost());
+		model = this.setList(model);
+		model.addAttribute("path", "create");
+		return "layout";
+	}
+
+	/**
+	* 一覧を設定する。
+	*
+	* @param model モデル
+	* @return 一覧を設定したモデル
+	*/
+	private Model setList(Model model) {
+		List<Post> list = repository.findAll();
+		model.addAttribute("list", list);
+		return model;
+	}
 }
